d8b33be0e8684175429862d2d3abe719838ee214
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-05-17 00:39:48 +0200

    Removes files from dist folder from the Git repository

--- packages/collaboration/comments/dist/findThreadNodeEntry.d.ts
+++
@@ -1,6 +0,0 @@
-import { TEditor } from '@udecode/plate-core';
-import { NodeEntry } from 'slate';
-import { Thread } from './Thread';
-import { ThreadNode } from './types';
-export declare function findThreadNodeEntry(editor: TEditor, thread: Thread): NodeEntry<ThreadNode> | null;
-//# sourceMappingURL=findThreadNodeEntry.d.ts.map
--- packages/collaboration/comments/dist/transforms/findThreadPath.d.ts
+++
@@ -1,7 +0,0 @@
-import { PEditor } from '@udecode/plate-core';
-import { Location } from 'slate';
-import { HistoryEditor } from 'slate-history';
-import { ReactEditor } from 'slate-react';
-import { Thread } from '../Thread';
-export declare function findThreadPath<T>(editor: PEditor<T> & HistoryEditor & ReactEditor & T, thread: Thread): Location | undefined;
-//# sourceMappingURL=findThreadPath.d.ts.map
