7b761a4fd0ca8119cb5e40b95e8a9483a0bc6abf
osamatanveer <osamatanveer@users.noreply.github.com>
2022-08-24 21:19:40 +0000

    docs

---
+++ docs/docs/sandpack/files/excalidraw/code-user.ts
@@ -0,0 +1,11 @@
+export const userCode = `export const user = {
+  id: '1',
+  name: 'Jon Doe',
+  email: 'jon.doe@example.com',
+  avatarUrl: '/img/avatar.svg',
+};
+`;
+
+export const userFile = {
+  '/excalidraw/user.ts': userCode,
+};
